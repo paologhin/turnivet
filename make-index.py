982785702ad174c3ce707d516aad46c96b3f6781
paologhin <92395873+paologhin@users.noreply.github.com>
2021-10-15 21:24:59 +0200

    update py script

--- make-index.py
+++ make-index.py
@@ -1,23 +1,42 @@
 import os
+from datetime import date
+
+today = date.today()
 
 f = open("index.html", "w")
 
-html_str = """
+html_str = ("""
 <!DOCTYPE html>
 <html>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<head>
+<link rel="stylesheet" href="https://paologhin.github.io/turnivet/style.css">
+</head>
 <body>
 
-<h2>TurniVet</h2>
-
+<div class="header-color">
+  <div class="header">
+  <img id="header-image" src="https://raw.githubusercontent.com/paologhin/turnivet/main/bovini.jpg">
+  <h1 id="title">TurniVet</h1>
+<p id="subtitle">Ultimo aggiornamento <b>""" + 
+today.strftime("%d/%m/%y") + "</b></p>\n" +
 """
+  </div> 
+</div>
+
+<table>
+""")
 
 for file in os.listdir("./"):
     if file.endswith(".html"):
         if file != "index.html":
-            html_str += "\n<A HREF=https://paologhin.github.io/turnivet/" + file + ">" + file.removesuffix('.html') + "</A>"
-            html_str += "\n<br><br>"
+            html_str += "\n<tr><td><A HREF=https://paologhin.github.io/turnivet/" + file + ">" + file.removesuffix('.html')
+            html_str += "</A></td></tr>"
 
 html_str += """
+</table>
 
 </body>
 </html>
@@ -25,3 +44,5 @@ html_str += """
 
 f.write(html_str)
 f.close()
+
+
